dff521e5c0a7508ac2777307dfaeaea5c89cb277
Ryan <ryanbusbyalt@gmail.com>
2017-07-30 23:57:32 -0400

    Changed from cli to asking for input

--- pyscraper.py
+++ pyscraper.py
@@ -1,18 +1,19 @@
 ''' YOU NEED TO DOWNLOAD PyPDF2 into whatever version of Python you're using '''
 
 import PyPDF2
-from sys import argv
+#from sys import argv
 
 # I didn't add in help files in this script. Basically:  First argument is book, second argument is page number.
 
-book = argv[1]
-page = argv[2]
-print(argv)
-with open(argv[1], 'rb') as pdfFileObj:
+book = input("Name of pdf (include .pdf): ")
+page = int(input("Page Number: "))
+        
+with open(book, 'rb') as pdfFileObj:
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    pageObj = pdfReader.getPage(int(argv[2]))
+    pageObj = pdfReader.getPage(page)
     print(pageObj.extractText())
     
     # This will show you the number of pages in the book
     pdfReader.numPages
     print(pdfReader.numPages)
+
